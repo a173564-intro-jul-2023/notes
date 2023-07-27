8c8a705edd253b90f483a936505f432b3dafbd33
Becky Melvin <rebecca_melvin@progressive.com>
2023-07-27 16:51:45 -0400

    Done with EmployeeDirectoryApi

---
+++ EmployeeDirectorySolution/EmployeeDirectoryApi/Data/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeDirectoryApi.Data;
+
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public decimal Salary { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+}
