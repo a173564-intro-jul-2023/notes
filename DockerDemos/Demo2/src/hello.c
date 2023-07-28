8fb149efb7390964f6fee04ecdc1866d6783f691
Becky Melvin <rebecca_melvin@progressive.com>
2023-07-28 12:29:31 -0400

    Docker demos

---
+++ DockerDemos/Demo2/src/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main (void) {
+    printf("Hello from a C application inside the container!!!\n");
+    return 0;
+}
---
+++ DockerDemos/Demo3/src/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main (void) {
+    printf("Hello from a C application inside the container with multistage build!!!\n");
+    return 0;
+}
